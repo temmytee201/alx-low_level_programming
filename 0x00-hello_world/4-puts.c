b137bc016b2f28c3f00f866e3d062dccf9763c8f
Temmytee201 <temitopeisulaiman@gmail.com>
2022-09-08 17:25:01 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ *  * main -entry point
+ *   *
+ *    * Return: 0 after printing the function
+ *     */
+#include <stdio.h>
+int main(void)
+{
+			puts("\"Programming is like building a multilingual puzzle");
+						return (0);
+}
