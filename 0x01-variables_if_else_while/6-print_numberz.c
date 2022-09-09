3bd7599252c26c0aed9144d5f12eab54b00fe22d
Temmytee201 <temitopeisulaiman@gmail.com>
2022-09-09 15:13:30 -0700

    print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *  * main -entry point
+ *   *
+ *    * function main - prints number 1 to 10
+ *     *
+ *      * Return: 0
+ *       */
+int main(void)
+{
+			int a = 0;
+
+					for (; a < 10 ; a++)
+									putchar('0' + a);
+							putchar('\n');
+									return (0);
+}
