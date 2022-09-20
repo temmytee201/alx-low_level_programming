4aa4c2f4835d194f579cb633ca03cf34865b3323
Temmytee201 <temitopeisulaiman@gmail.com>
2022-09-20 14:21:10 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*  * _atoi - converts a string to an integer
-*   * @s: string to convert
-*    *
-*     * Return: value of integer
-*/
+ *  * _atoi - converts a string to an integer
+ *   * @s: string to convert
+ *    *
+ *     * Return: value of integer
+ *      */
 int _atoi(char *s)
 {
 		int i, j, n, m;
