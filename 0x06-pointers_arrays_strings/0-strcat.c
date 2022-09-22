0a04efe04a0849832a8df0617b1b8f3a21f973d8
Temmytee201 <temitopeisulaiman@gmail.com>
2022-09-22 13:57:58 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,17 @@
 */
 char *_strcat(char *dest, char *src)
 {
-		int i, j;
+	int i, j;
 
-			i = j = 0;
-				while (*(dest + i))
-							i++;
-					while ((*(dest + i) = *(src + j)))
+	i = j = 0;
+	while (*(dest + i))
+	i++;
+	while ((*(dest + i) = *(src + j)))
 
 
-					{
-										i++;
-												j++;
-													}
-						return (dest);
+	{
+		i++;
+		j++;
+				}
+		return (dest);
 }
